221bcdb8ba69ddf4caab2c93cc0c29bcb307572c
rushtongarth <stephen.garth@gmail.com>
2020-03-05 06:58:57 -0600

    removal of context methods from google obj

--- src/__init__.py
+++ src/__init__.py
@@ -13,39 +13,11 @@ class GoogleObj(object):
         self.qstr = kwargs.get('query', qstr)
         self.user = "me"
 
-    def __enter__(self):
+    @property
+    def service(self):
         with open(credentials, 'rb') as f:
             creds = pkl.load(f)
-        self.service = build('gmail', 'v1', credentials=creds)
-        self.msgs = self.service.users().messages()
-        return self
-
-    def __exit__(self, *args):
-        pass
-
-    #def __mids(self):
-        #"""pull all message ids from email address"""
-        #messages = []
-        #kw = dict(userId=self.user, q=self.qstr)
-        #_msgs = self.msgs.list(**kw)
-        #msgs = _msgs.execute()
-        #if 'messages' in msgs:
-            #messages.extend(msgs['messages'])
-        #while 'nextPageToken' in msgs:
-            #kw['pageToken'] = msgs['nextPageToken']
-            #msgs = self.msgs.list(**kw).execute()
-            #messages.extend(msgs['messages'])
-        #ids = map(op.itemgetter('id'), messages)
-        #self._mids = np.fromiter(ids, dtype=(str, 16))
-
-    #@property
-    #def mids(self):
-        #if hasattr(self, '_mids'):
-            #self.__mids()
-        #return self._mids
-
-    #def __enter__(self):
-        ## load available ids to object assuming everything else worked
-        #self.__mids()
-        #return self
-
+        return build('gmail', 'v1', credentials=creds) 
+    @property
+    def messages(self):
+        return self.service.users().messages() 
